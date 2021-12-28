1a6ec3718d7f4cfb638ecbac441194cf6b4ea351
Jan Damm <mail@jandamm.de>
2021-12-28 23:21:49 +0100

    Cleanup lazy loading autoupdate (#107)

--- zgenom.zsh
+++ zgenom.zsh
@@ -15,10 +15,9 @@ fpath=($ZGEN_SOURCE/functions $fpath)
 autoload -Uz __zgenom
 autoload -Uz __zgenom_out
 autoload -Uz __zgenom_err
-autoload -Uz zgenom-autoupdate
 function zgenom() {
     case $1 in
-    autoupdate) shift; zgenom-autoupdate $@;;
+    autoupdate) shift; autoload -Uz zgenom-autoupdate && zgenom-autoupdate $@;;
     init) zgenom saved || return 0;;
     # $ZGEN_INIT might delete itself when $ZGEN_RESET_ON_CHANGE is used.
     saved) [[ -f "${ZGEN_INIT}" ]] && source ${ZGEN_INIT} && [[ -f "${ZGEN_INIT}" ]];;
